a09cdebffd068b3b90c318c8e5d22bc1422f00f9
alexis-lc <alexis.lecabellec@gmail.com>
2024-04-26 11:52:41 +0200

    turns out I cant read

--- app/routes/api.month-stats.tsx
+++ app/routes/api.month-stats.tsx
@@ -16,8 +16,8 @@ export const config = {
     const bzh = {lat: 48.168656, lon: -2.966861}
     const bzhRqst = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${bzh.lat}&lon=${bzh.lon}&appid=${process.env.OPENWEATHERMAP_KEY}`)
     const bzhAsw = await bzhRqst.json()
-    console.log("[MONTHS STATS] Current weather in Brittany : " + bzhAsw.weather.main)
-    const bzhRain = bzhAsw.weather.main == "Thunderstorm" || bzhAsw.weather.main == "Drizzle" || bzhAsw.weather.main == "Rain"
+    console.log("[MONTHS STATS] Current weather in Brittany : " + bzhAsw.weather[0].main)
+    const bzhRain = bzhAsw.weather[0].main == "Thunderstorm" || bzhAsw.weather[0].main == "Drizzle" || bzhAsw.weather[0].main == "Rain"
     const serverData = await kv.get<{isRaining: boolean, last_updated: number, rainOccurencesBzh: number, rainOccurencesNmd: number, lastExecMonth: number}>('weather')
     if(bzhRain && serverData != null) {
 
@@ -28,8 +28,8 @@ export const config = {
     const nmd = {lat: 49.220917, lon: 0.354484}
     const nmdRqst = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${nmd.lat}&lon=${nmd.lon}&appid=${process.env.OPENWEATHERMAP_KEY}`)
     const nmdAsw = await nmdRqst.json()
-    console.log("[MONTHS STATS] Current weather in Normandy : " + nmdAsw.weather.main)
-    const nmdRain = nmdAsw.weather.main == "Thunderstorm" || nmdAsw.weather.main == "Drizzle" || nmdAsw.weather.main == "Rain"
+    console.log("[MONTHS STATS] Current weather in Normandy : " + nmdAsw.weather[0].main)
+    const nmdRain = nmdAsw.weather[0].main == "Thunderstorm" || nmdAsw.weather[0].main == "Drizzle" || nmdAsw.weather[0].main == "Rain"
     
     if(nmdRain && serverData != null) {
       serverData.rainOccurencesNmd += 1
